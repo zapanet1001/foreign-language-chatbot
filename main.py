18cad988c87d472a9d4b17a760d8bad3ea54e056
zapanet1001 <ipolo1969@ufl.edu>
2019-11-06 21:33:25 -0500

    added a cutoff for response accruacy

--- main.py
+++ main.py
@@ -103,14 +103,17 @@ def chat():
         if inp.lower() == "quit":
             break
 
-        results = model.predict([bag_of_words(inp,words)])
+        results = model.predict([bag_of_words(inp,words)])[0]
         results_index = numpy.argmax(results)
         tag = labels[results_index]
-        
-        for tg in data["intents"]:
-            if tg['tag'] == tag:
-                responses = tg['responses']
-        
-        print(random.choice(responses))
+
+        if results[results_index] > 0.7:
+            for tg in data["intents"]:
+                if tg['tag'] == tag:
+                    responses = tg['responses']
+            
+            print(random.choice(responses))
+        else:
+            print("I don't quite understand, try again.")
 
 chat()
